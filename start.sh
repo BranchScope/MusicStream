5c82b8558e46019e1e897c8e25248c45e2f21fac
BranchScope <39023184+BranchScope@users.noreply.github.com>
2020-12-15 20:22:17 +0000

    fixes and improvements
    - removed some songs
    - fixed $THREADS var
    - made more order :)

--- start.sh
+++ start.sh
@@ -7,12 +7,14 @@ YOUTUBE_URL="rtmp://x.rtmp.youtube.com/live2" #usually it's this, but change if 
 YOUTUBE_KEY="" #insert here your youtube key
 VIDEO_SOURCE="background.png" #default background you can see in the main path, change it if you want
 AUDIO_ENCODER="aac" #audio encoder :)
-THREADS = 5 #put cores +1
+THREADS="5" #put cores +1
 
 echo "Updating the list..."
-cd songs
 rm list.txt
-php kek.php
+cd songs
+ls *.mp3 | sort -R |tail -$N | while read file; do
+    echo "file 'songs/$file'" >> ../list.txt
+done
 cd ..
 echo "Starting the streaming..."
 
@@ -24,8 +26,8 @@ ffmpeg \
  -thread_queue_size 512 \
  -f concat \
  -safe 0 \
- -i songs/list.txt \
+ -i list.txt \
  -loop -1 \
  -c:v libx264 -tune stillimage -pix_fmt yuv420p -preset $QUAL -r $FPS -g $(($FPS *2)) -b:v $VBR \
  -c:a $AUDIO_ENCODER -threads $THREADS -ar 44100 -b:a 128k -bufsize 512k -pix_fmt yuv420p \
- -f flv $YOUTUBE_URL/$YOUTUBE_KEY 
+ -f flv $YOUTUBE_URL/$YOUTUBE_KEY
